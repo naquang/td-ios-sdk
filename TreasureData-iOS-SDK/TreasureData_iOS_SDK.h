2ebb67fb61479120676b53b730d3331882219007
quang <naqgate@gmail.com>
2019-09-21 02:19:19 +0900

    first commit

---
+++ TreasureData-iOS-SDK/TreasureData_iOS_SDK.h
@@ -0,0 +1,19 @@
+//
+//  TreasureData_iOS_SDK.h
+//  TreasureData-iOS-SDK
+//
+//  Created by Nganwashere on 2019/09/21.
+//  Copyright © 2019 Nganwashere. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TreasureData_iOS_SDK.
+FOUNDATION_EXPORT double TreasureData_iOS_SDKVersionNumber;
+
+//! Project version string for TreasureData_iOS_SDK.
+FOUNDATION_EXPORT const unsigned char TreasureData_iOS_SDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TreasureData_iOS_SDK/PublicHeader.h>
+
+
